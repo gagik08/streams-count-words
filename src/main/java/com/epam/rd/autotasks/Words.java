579eeaddc4271dbc313e6e6fe5d57ed69c583b11
Gagik Kazandjyan <bi0820a@outlook.com>
2023-07-21 01:14:20 +0400

    solved

--- src/main/java/com/epam/rd/autotasks/Words.java
+++ src/main/java/com/epam/rd/autotasks/Words.java
@@ -1,12 +1,30 @@
 package com.epam.rd.autotasks;
 
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class Words {
 
     public String countWords(List<String> lines) {
+        String[] words = String.join("\n", lines).toLowerCase().replaceAll("[^\\p{L}\\p{N}]+", " ").split("\\b");
 
-        return null;
+        Map<String, Long> wordCounts = Arrays.stream(words)
+                .collect(Collectors.groupingBy(word -> word, Collectors.counting()));
+
+        Map<String, Long> filteredWords = wordCounts.entrySet().stream()
+                .filter(entry -> entry.getKey().length() >= 4 && entry.getValue() >= 10)
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+
+        List<Map.Entry<String, Long>> sortedWords = filteredWords.entrySet().stream()
+                .sorted(Map.Entry.<String, Long>comparingByValue().reversed()
+                        .thenComparing(Map.Entry.comparingByKey()))
+                .collect(Collectors.toList());
+
+        return sortedWords.stream()
+                .map(entry -> entry.getKey() + " - " + entry.getValue())
+                .collect(Collectors.joining("\n"));
     }
 }
