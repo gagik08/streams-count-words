02d492947171c63c11100cd5558cc1aba53824c5
Gagik Kazandjyan <bi0820a@outlook.com>
2023-08-03 16:20:14 +0400

    corrected (4th attempt)

--- src/main/java/com/epam/rd/autotasks/Words.java
+++ src/main/java/com/epam/rd/autotasks/Words.java
@@ -6,28 +6,31 @@ import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Predicate;
-import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.*;
+
 
 public class Words {
 
     public String countWords(List<String> lines) {
         Predicate<String> lengthIsEqualOrMoreThan4 = (word) -> word.length() >= 4;
         List<String> words = lines.stream()
-                .flatMap(str -> Arrays.stream(str.replaceAll("[^\\p{L}\\p{N}]+", " ").trim().split("\\s+")))
+                .flatMap(str -> Arrays.stream(str.split("[^\\p{L}\\p{N}]+")))
                 .filter(lengthIsEqualOrMoreThan4)
                 .map(String::toLowerCase)
-                .collect(Collectors.toList());
+                .collect(toList());
 
+
+        Predicate<Map.Entry<String, Long>> occuresMoreOrEqualTo10Times = entry -> entry.getValue() >= 10;
         Map<String,Long> frequency = words.stream()
-                .collect(Collectors.groupingBy(word -> word, Collectors.counting()))
+                .collect(groupingBy(word -> word, counting()))
                 .entrySet().stream()
-                .filter(entry -> entry.getValue() >= 10)
+                .filter(occuresMoreOrEqualTo10Times)
                 .sorted(Map.Entry.<String, Long>comparingByValue().reversed().thenComparing(Map.Entry.comparingByKey()))
-                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (oldValue, newValue) -> oldValue, LinkedHashMap::new));
+                .collect(toMap(Map.Entry::getKey, Map.Entry::getValue, (oldValue, newValue) -> oldValue, LinkedHashMap::new));
 
         StringBuilder stringBuilder = new StringBuilder();
         frequency.forEach((word, count) -> stringBuilder.append(word).append(" - ").append(count).append('\n'));
         return stringBuilder.toString().trim();
-
     }
 }
